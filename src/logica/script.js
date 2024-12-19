df8f36010bc385f232cc11eb835e32f28e0271f9
carmo-montana <carmodepp@gmail.com>
2024-12-19 20:01:16 -0300

    Melhorando o site

--- src/logica/script.js
+++ src/logica/script.js
@@ -1,28 +1,38 @@
 const submitBtn = document.getElementById("submitBtn");
 const senhaInput = document.getElementById("senha");
+const mensagem = document.querySelector(".message");
 
-submitBtn.addEventListener("click", () => {
-    const senha = senhaInput.value;
 
-    if (senha === "1337") {
-        window.location.href = "enigmaImg.html"
-    } else {
-        alert("Senha incorreta.")
+// submitBtn.addEventListener("keypress", () => {
+//     const senha = senhaInput.value;
+
+//     if (senha === "1337") {
+//         window.location.href = "enigmaImg.html"
+//     } else {
+//         alert("Senha incorreta.")
+//     }
+
+// })
+
+senhaInput.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+        const password = senhaInput.value;
+
+        if (checksenha(password)) {
+            window.location.href = "enigmaImg.html"
+        } else {
+            mensagem.textContent = "Senha incorreta. Tente novamente.";
+            mensagem.style.color = "#fff"
+            senhaInput.value = '';
+
+            setTimeout(() => {
+                mensagem.style.block = "none";
+            }, 3000)
+        }
     }
 })
 
 
-// const botao = document.addEventListener("DOMContentLoaded", () => {
-//     const senhaInput = document.getElementById("senha");
-
-//     senhaInput.addEventListener("submit", (e) => {
-//         e.preventDefault();
-//         senhaInput.classList.add("sending");
-//     })
-
-//     setTimeout(() => {
-//         senhaInput.reset();
-//         senhaInput.remove("sending");
-//         alert("Você conseguiu parabéns.")
-//     }, 2000)
-// })
+function checksenha(password) {
+    return password === "1337";
+}
